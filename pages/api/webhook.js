9f3254824f867c21a663e88c67bc56280f5a50bd
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-11-23 12:22:27 -0600

    new webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,14 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default function handler(req, res) {
-  res.json({
-
-    action: req.query.action,
-    api_version: req.query.api_version,
-    application_id: req.query.application_id,
-    date_created: req.query.date_created,
-    id: req.query.id,
-    live_mode: req.query.live_mode,
-    type: req.query.type,
-    user_id: req.query.user_id,
-  });
+  console.log(req.query);
+  res.status(200)
 }
